864c31a66464740bce7f6dcee0be4fb40b5a0807
Asbkv <nazymasylbekov@gmail.com>
2025-06-24 12:16:29 +0600

    sort-product

--- src/components/pages/product/Product.jsx
+++ src/components/pages/product/Product.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import "./Product.scss";
 import { useDispatch, useSelector } from "react-redux";
 import axios from "axios";
@@ -6,16 +6,33 @@ import ProductCard from "../../ui/productCard/ProductCard";
 
 const Product = () => {
   const product = useSelector((state) => state.product);
-  console.log(product);
 
   const dispatch = useDispatch();
+  const [find, setFind] = useState([]);
 
   const getProducts = async () => {
     const res = await axios.get(
       `https://api-crud.elcho.dev/api/v1/11d9b-bcdb1-01943/bagProduct`
     );
-    dispatch({ type: "GET_PRODUCTS", payload: res.data.data });
+    const { data } = res.data;
+    dispatch({
+      type: "GET_PRODUCTS",
+      payload: data,
+    });
+    setFind(data);
   };
+
+  function getFind(el) {
+    if (el === "expensive") {
+      const sorted = [...product].sort((a, b) => b.price - a.price);
+      setFind(sorted);
+    } else if (el === "cheap") {
+      const sorted = [...product].sort((a, b) => a.price - b.price);
+      setFind(sorted);
+    } else {
+      setFind(product);
+    }
+  }
 
   useEffect(() => {
     getProducts();
@@ -24,9 +41,24 @@ const Product = () => {
   return (
     <div id="product">
       <div className="container">
+        <select
+          onChange={(e) => getFind(e.target.value)}
+          defaultValue=""
+          style={{
+            fontSize: "23px",
+            border: "none",
+            padding: "10px",
+            marginBottom: "30px",
+            borderRadius: "10px",
+          }}
+        >
+          <option value="">All</option>
+          <option value="expensive">Expensive</option>
+          <option value="cheap">Cheap</option>
+        </select>
         <div className="product">
-          {product.map((el) => (
-          <ProductCard el={el} key={el._id}/>
+          {find?.map((el, idx) => (
+            <ProductCard el={el} key={idx} />
           ))}
         </div>
       </div>
@@ -35,6 +67,3 @@ const Product = () => {
 };
 
 export default Product;
-
-
-
